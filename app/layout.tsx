085b47f170fbf7ad8285775b66f40271012b1334
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2023-09-01 11:17:44 +0800

    Added Skills.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" className="!scroll-smooth">
       <body
-        className={`bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 h-[2000px] min-w-[390px] sm:min-[initial]:
-      ${inter.className}`}>
+        className={`bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 h-[2000px] min-w-[390px] 
+        ${inter.className}`}>
         <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[-11rem] h-[31.25rem] w-[31.25rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem]" />
         <div className="bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left[-5rem]" />
         <ActiveSectionContextProvider>
---
+++ components/skills.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import React from 'react'
+import { motion } from 'framer-motion'
+import SectionHeading from './section-heading'
+import { skillsData } from '@/lib/data'
+import { useSectionInView } from '@/lib/hooks'
+
+const fadeInAnimationVariants = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: (idx: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: { delay: 0.05 * idx },
+  }),
+}
+
+export default function Skills() {
+  const { ref } = useSectionInView('Skills')
+
+  return (
+    <section
+      id="skills"
+      ref={ref}
+      className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40 ">
+      <SectionHeading>My Skills</SectionHeading>
+      <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
+        {skillsData.map((item, idx) => (
+          <motion.li
+            key={idx}
+            className="px-5 py-3 bg-white border border-black/10 rounded-xl"
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            animate="animate"
+            whileInView="animate"
+            viewport={{ once: true }}
+            custom={idx}>
+            {item}
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  )
+}
